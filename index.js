8fae7e3f573ba519950e1ca2d00e420179599528
Flower <68172262+NoBrain0917@users.noreply.github.com>
2020-11-07 10:17:29 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const request = require("sync-request");
+
+getYoutubeVideoURI = (url) => {
+	let urlSplit = url.split("/");
+  let urlCode = urlSplit[urlSplit.length-1];
+  let res = request("GET","https://www.youtube.com/get_video_info?video_id="+urlCode);
+  let videoJson = JSON.parse(decodeURIComponent(unescape(String(res.body).replace(/\\/g,"%"))).split('formats":[')[1].split("}")[0]+"}");
+  return videoJson.url;
+}
